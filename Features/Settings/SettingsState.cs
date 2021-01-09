ec9432c2c1eb2eb460f6d150f44b22e1dceaef13
Todd Schavey <schaveyt@gmail.com>
2021-01-08 21:56:02 -0500

    update ui after answering question

--- Features/Settings/SettingsState.cs
+++ Features/Settings/SettingsState.cs
@@ -33,8 +33,27 @@ namespace QuizTime.Features.Settings
             }
         }
 
+        public ThemeColor TrinaryColor
+        {
+            get
+            {               
+                switch (this.ThemeColor)
+                {
+                    case ThemeColor.Green:
+                        return ThemeColor.Pink;
+                    case ThemeColor.Blue:
+                        return ThemeColor.Green;
+                    case ThemeColor.Pink:
+                        return ThemeColor.Blue;
+                    default:
+                        return ThemeColor.Undefined;
+                }
+            }
+        }
+
         public string ThemeColorString => ThemeColor.ToString().ToLower();
         public string SecondaryColorString => SecondaryColor.ToString().ToLower();
+        public string TrinaryColorString => TrinaryColor.ToString().ToLower();
 
         public override void Initialize()
         {
