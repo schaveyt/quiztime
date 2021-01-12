8a3ad4208d7ddbc742675823bc0bd3578a14dd36
Todd Schavey <schaveyt@gmail.com>
2021-01-12 17:27:10 -0500

    reorg project structure and add featherhttp

---
+++ QuizTime.Api.Rest/Program.cs
@@ -0,0 +1,21 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace data.api
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var app = WebApplication.Create(args);
+
+            app.MapGet("/", async http =>
+            {
+                await http.Response.WriteAsync("Hello World!");
+            });
+
+            await app.RunAsync();
+        }
+    }
+}
