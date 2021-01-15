d52f6c0b5cf24ffa946199b65cd01d486bc9c08a
Todd Schavey <schaveyt@gmail.com>
2021-01-15 00:34:43 -0500

    add success images and display

--- QuizTime.Client.BlazorWasm/Shared/Data/QuizDataService.cs
+++ QuizTime.Client.BlazorWasm/Shared/Data/QuizDataService.cs
@@ -13,7 +13,7 @@ namespace QuizTime.Client.BlazorWasm.Shared.Data
         [Inject]
         public HttpClient Http { get; set; }
 
-        private static readonly SortedList<string, Player> _testplayers = new SortedList<string, Player>
+        private readonly SortedList<string, Player> _testplayers = new SortedList<string, Player>
         {
             { "Mia", new Player (){ Name = "Mia", MinLevel = 0, MaxLevel = 2, Color = ThemeColor.Blue } },
             { "Izzy", new Player (){ Name = "Izzy", MinLevel = 0, MaxLevel = 2, Color = ThemeColor.Pink } },
@@ -21,6 +21,29 @@ namespace QuizTime.Client.BlazorWasm.Shared.Data
             { "Mom", new Player (){ Name = "Mom", MinLevel = 11, MaxLevel = 16, Color = ThemeColor.Yellow } },
             { "Dad", new Player (){ Name = "Dad", MinLevel = 11, MaxLevel = 16, Color = ThemeColor.Purple } },
         };
+
+        private readonly List<string> _successImages = new List<string>()
+        {
+            "https://media.giphy.com/media/BSx6mzbW1ew7K/giphy.gif", // nyan cat animated
+            "https://media.giphy.com/media/mtaWx98w7mX7y/giphy.gif", // nyan cat pikachu
+            "https://media.giphy.com/media/l41lZxzroU33typuU/giphy.gif", // full house you got it dude
+            "https://media.giphy.com/media/bTzFnjHPuVvva/giphy.gif", // carlton
+            "https://media.giphy.com/media/j3gsT2RsH9K0w/giphy.gif", // nepoleaon dynamite dance
+            "https://media.giphy.com/media/SKGo6OYe24EBG/giphy.gif", // spongebob rainbow
+            "https://media.giphy.com/media/o75ajIFH0QnQC3nCeD/giphy.gif", // the office fist pump
+            "https://media.giphy.com/media/111ebonMs90YLu/giphy.gif", // kid thumbs up
+            "https://media.giphy.com/media/Swx36wwSsU49HAnIhC/giphy.gif", // dumbledore applause
+            "https://media.giphy.com/media/26gN16cJ6gy4LzZSw/giphy.gif", // harry potter applause
+            "https://media.giphy.com/media/3otPoS81loriI9sO8o/giphy.gif", // elf you did it
+            "https://media.giphy.com/media/3o7abKhOpu0NwenH3O/giphy.gif", // spungebob thumbs up
+            "https://media.giphy.com/media/oobNzX5ICcRZC/giphy.gif", //minion thumbs up
+            "https://media.giphy.com/media/1gqDQUaLe3mCc/giphy.gif", // awesome
+            "https://media.giphy.com/media/3o7abGQa0aRJUurpII/giphy.gif", // spongebob many thumbup
+            "https://media.giphy.com/media/5xtDarziy6FezwXWCbu/giphy.gif", // uncle grandpa pizza
+            "https://media.giphy.com/media/kMxknXFU9P8d2/giphy.gif", // my little pony 
+        };
+        
+        public List<string> SuccessImages => _successImages;
 
         public async Task<IQuizItem> GetNextQuizItem(uint minSkillLevel, uint maxSkillLevel)
         {
